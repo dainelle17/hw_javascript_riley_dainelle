bbe2a845fab88ef42f14c3c5695acd76ae76062a
Dainelle Riley <dainellecriley@ufl.edu>
2015-09-25 15:41:55 -0400

    input functions

--- js/main.js
+++ js/main.js
@@ -51,3 +51,35 @@ var RonaldReagan = {
 }
 
 ]
+
+function myfunction()	{
+	console.log(presidents [3],Clinton,Bill);
+}
+
+//Functions//
+function howdy ()	{
+	console.log ('Dainelle Riley says whats up!')
+}
+
+var numba = function ('string') {
+	if (string , numba)	{
+		console.log ('What a short little word!');
+
+	else if (string > numba) {
+		console.log ('I am sorry, but thats too many to count')
+	
+	}
+	else if (string = numba) {
+		console.log ('What a perfect choice!');
+	}
+
+function numba (string) {
+	printToConsole ('7')
+	printToConsole ('seventy')
+	printToConsole ('university')
+}
+
+function inception (callback, value) {
+	callback (value);
+	console.log ('Bridesmaids is a great movie!')
+}
