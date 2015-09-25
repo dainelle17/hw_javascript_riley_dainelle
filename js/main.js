ad0e85d15117076763b02c83ae8af1552b9b9937
Dainelle Riley <dainellecriley@ufl.edu>
2015-09-25 15:09:23 -0400

    input president objects

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//Array//
+
+var iceCream = [
+	'chocolate', 
+	'vanilla', 
+	'cheesecake'
+	iceCream.push ('cake batter'); 
+	]
+
+// Objects //
+
+var presidents = {
+
+var BarackObama = { 
+	firstName = 'Barack'
+	lastName = 'Obama'
+	termLength = 'two terms'
+	party = 'Democrat'
+	yearsOfPresidency = '2009-current'
+}
+var GeorgeWBush = {
+	firstName = 'George'
+	lastName = 'Bush'
+	termLength = 'two terms'
+	party = 'Republican'
+	yearsOfPresidency = '2001-2009'
+}
+
+var BillClinton = {
+	firstName = 'Bill'
+	lastName = 'Clinton'
+	termLength = 'two terms'
+	party = 'Democrat'
+	yearsOfPresidency = '1993-2001'
+}	
+
+var GeorgeHBush = {
+	firstName = 'George'
+	lastName = 'Bush'
+	termLength = 'one term'
+	party = 'Republican'
+	yearsOfPresidency = '1989-1993'
+}
+
+var RonaldReagan = {
+	firstName = 'Ronald'
+	lastName = 'Reagan'
+	termLength = 'two terms'
+	party = 'Republican'
+	yearsOfPresidency = '1981-1989'
+}
+
+}
